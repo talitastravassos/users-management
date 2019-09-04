a6299ef3cfa56b2b9e72f2fb8fb77c7a3ee4ab9e
talitast <talitatravassos.tt@gmail.com>
2019-09-04 12:56:45 -0300

    initial cleaning

---
+++ src/containers/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Table from '../../components/Table';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Table />
+        </div>
+    )
+}
+
+export default Home
